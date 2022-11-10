35c454b4c11eb6275b2c3e879c144bf9e2e8ccbc
Alphée GROSDIDIER <alphee.grosdidier@gmail.com>
2022-11-10 17:25:19 +0100

    Create docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -7,7 +7,7 @@
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
 project = 'CyberArena'
-copyright = '2022, Yasser  OMARI, Alphée GROSDIDIER, Fantin GAUTHIER'
+copyright = '2022, Yasser OMARI, Alphée GROSDIDIER, Fantin GAUTHIER'
 author = 'Yasser OMARI, Alphée GROSDIDIER, Fantin GAUTHIER'
 release = 'pre-alpha'
 
@@ -27,3 +27,7 @@ exclude_patterns = [
 
 html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
+
+html_title = 'CyberArena'
+html_logo = None
+html_favicon = None
