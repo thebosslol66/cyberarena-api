96da787e21e7108a27446d42c1883c76bc0c0a19
Alphée GROSDIDIER <alphee.grosdidier@gmail.com>
2022-11-16 18:58:24 +0100

    Initial commit

---
+++ cyberarena/db/models/__init__.py
@@ -0,0 +1,14 @@
+"""cyberarena models."""
+import pkgutil
+from pathlib import Path
+
+
+def load_all_models() -> None:
+    """Load all models from this folder."""
+    package_dir = Path(__file__).resolve().parent
+    modules = pkgutil.walk_packages(
+        path=[str(package_dir)],
+        prefix="cyberarena.db.models.",
+    )
+    for module in modules:
+        __import__(module.name)  # noqa: WPS421
