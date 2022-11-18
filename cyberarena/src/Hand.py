ae2ce73d13ddfffaec8c1fb7b5769e14d6fb20a8
FT1Gauthier <fantin.gauthier@gmail.com>
2022-11-18 18:18:45 +0100

    card deck hand

--- cyberarena/src/Hand.py
+++ cyberarena/src/Hand.py
@@ -1,13 +1,22 @@
+from typing import List
+
+from cyberarena.src import Card, Deck
+
+
 class Hand :
     def __init__(self) :
-        self.card = []
-
-    def getFirstHand(self):
+        self.card:List[Card] = []
+    def getFirstHand(self) -> None:
         for x in range(0,10) :
             self.card.append(Deck.getRandomCard())
-
-    def getNextCard(self):
-        self.card.append(Deck.getRandomCard())
-
-    def getCardFromHand(self,card):
+    def getNextCard(self) -> None:
+        card : Card = Deck.getRandomCard()
+        if card != None:
+            self.card.append(Deck.getRandomCard())
+    def useCard(self,card : Card) -> Card :
+        for x in self.card:
+            if x == card:
+                xsave = x
+                self.card.remove(x)
+                return xsave
 
