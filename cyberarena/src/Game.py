3d584d4db6b0220064e2916249680a88ea5c1631
Awennnnnn <78376598+Awennnnnn@users.noreply.github.com>
2022-11-21 22:57:17 +0100

    Game

--- cyberarena/src/Game.py
+++ cyberarena/src/Game.py
@@ -1,28 +1,35 @@
-
-
-class Game :
-    def __init__(self, id, player1, player2, terrain) :
+import logging
+LOGGER = logging.getLogger("game")
+class Game:
+    def __init__(self, id, player1, player2, terrain) -> None:
         self.player1 = player1
         self.player2 = player2
-        self.board = board
+        self.board = terrain
         self.turn = 0
         self.winner = None
         self.id = id
 
-    def play(self) :
-        while self.winner is None :
+    def play(self) -> None:
+        while self.winner is None:
             self.turn += 1
-            if self.turn % 2 == 1 :
+            if self.turn % 2 == 1:
                 self.player1.play(self.board)
-            else :
+            else:
                 self.player2.play(self.board)
             self.winner = self.board.check_winner()
 
-    def print_winner(self) :
-        if self.winner is None :
-            print("No winner")
-        else :
-            print("The winner is " + self.winner)
+    def print_winner(self) -> None:
+        if self.winner is None:
+            LOGGER.debug("No winner")
+        else:
+            LOGGER.debug(self.winner.name + " won the game")
 
-    def __str__(self):
-        return "Game between " + self.player1.name + " and " + self.player2.name + " on " + self.board.name
+    def __str__(self) -> str:
+        return (
+            "Game between "
+            + self.player1.name
+            + " and "
+            + self.player2.name
+            + " on "
+            + self.board.name
+        )
