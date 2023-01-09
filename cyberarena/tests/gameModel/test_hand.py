22d29ff0b92e65e2c07776ab1ce94f4a45952c76
FT1Gauthier <fantin.gauthier@gmail.com>
2023-01-09 17:19:04 +0100

    test hand

--- cyberarena/tests/gameModel/test_hand.py
+++ cyberarena/tests/gameModel/test_hand.py
@@ -12,3 +12,5 @@ async def test_hand() -> None:
     assert hand.get_hand_size() == 0
     hand.get_random_card()
     assert hand.get_hand_size() == 1
+    hand.use_card_debug(0)
+    assert hand.get_hand_size() == 0
