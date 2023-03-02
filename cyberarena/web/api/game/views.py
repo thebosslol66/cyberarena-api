bdf7cb751e78ddbfd6498f7e5842a113ce9580bf
Awennnnnn <78376598+Awennnnnn@users.noreply.github.com>
2023-03-03 00:04:46 +0100

    room_id updated when both players find a match

--- cyberarena/web/api/game/views.py
+++ cyberarena/web/api/game/views.py
@@ -3,7 +3,7 @@ from fastapi.security import SecurityScopes
 from fastapi.websockets import WebSocket
 from starlette import status
 from starlette.responses import FileResponse
-
+from loguru import logger
 from cyberarena.db.models.user_model import UserModel
 from cyberarena.web.api.connection.utils import get_current_user
 from cyberarena.web.api.game.schema import CardModel, TicketModel, TicketStatus
@@ -119,19 +119,21 @@ async def get_ticket_status(
     :return: The status of the ticket
     :raises HTTPException: If the ticket doesn't exist or is closed
     """
-    id_game = ticket_manager.find_match()
     # in the background
     statu: TicketStatus = ticket_manager.get_ticket_status(ticket_id)
+    logger.error("statu : " + statu)
     if statu == TicketStatus.DONT_EXIST:
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST,
             detail="Ticket doesn't exist.",
         )
-    if (statu == TicketStatus.CLOSED) and current_user.id:
-        id_user: int = current_user.id
-        ticket = ticket_manager.get_ticket_by_user_id(id_user)
-        if ticket:
-            id_game = get_game_id(ticket.ticket_id)
+    if statu == TicketStatus.CLOSED:
+        id_game = get_game_id(current_user.id)
+    elif statu == TicketStatus.OPEN:
+        id_game = ticket_manager.find_match()
+    else:
+        id_game = -1
+    statu = ticket_manager.get_ticket_status(ticket_id)
     return TicketModel(
         id=ticket_id,
         status=statu,
