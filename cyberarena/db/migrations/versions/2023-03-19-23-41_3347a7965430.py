b5a58594b205b023e8c81b11b6d5ab6e5032efc0
Alphée GROSDIDIER <alphee.grosdidier@gmail.com>
2023-03-20 00:52:23 +0100

    Forgetting the new versions files for migrator
    Signed-off-by: Alphée GROSDIDIER <alphee.grosdidier@gmail.com>

---
+++ cyberarena/db/migrations/versions/2023-03-19-23-41_3347a7965430.py
@@ -0,0 +1,21 @@
+"""empty message
+
+Revision ID: 3347a7965430
+Revises:
+Create Date: 2023-03-19 23:41:17.121254
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = "3347a7965430"
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
